342690f423740ac278937d97bdaa950e2164297c
yaoyue <2359240697@qq.com>
2022-05-04 13:35:03 +0800

    dialog页面数据填充

--- pages/dialog/dialog.js
+++ pages/dialog/dialog.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: "张三儿er",
+    garde: [66, 70, 98, 109, 343],
     message: "小姚，我的政治在85之上吗",
     index: 0,
     dialogData: [] //ture是左边数据，false是右边数据 
@@ -24,7 +26,48 @@ Page({
     })
   },
   tellMeHowToReturn: function (message) {
-    return message //todo
+    const name = this.data.name
+    if (message.search("政治") != -1) {
+      const messageGrade = message.match(/[0-9]+/)[0]
+      if (this.data.garde[0] >= messageGrade) {
+        return name + "，你的政治在" + messageGrade + "之上"
+      } else {
+        return name + "，你的政治在" + messageGrade + "之下"
+      }
+    }
+    if (message.search("英语") != -1) {
+      const messageGrade = message.match(/[0-9]+/)[0]
+      if (this.data.garde[1] >= messageGrade) {
+        return name + "，你的英语在" + messageGrade + "之上"
+      } else {
+        return name + "，你的英语在" + messageGrade + "之下"
+      }
+    }
+    if (message.search("数学") != -1) {
+      const messageGrade = message.match(/[0-9]+/)[0]
+      if (this.data.garde[2] >= messageGrade) {
+        return name + "，你的数学在" + messageGrade + "之上"
+      } else {
+        return name + "，你的数学在" + messageGrade + "之下"
+      }
+    }
+    if (message.search("专业") != -1) {
+      const messageGrade = message.match(/[0-9]+/)[0]
+      if (this.data.garde[3] >= messageGrade) {
+        return name + "，你的专业在" + messageGrade + "之上"
+      } else {
+        return name + "，你的专业在" + messageGrade + "之下"
+      }
+    }
+    if (message.search("总分") != -1) {
+      const messageGrade = message.match(/[0-9]+/)[0]
+      if (this.data.garde[4] >= messageGrade) {
+        return name + "，你的总分在" + messageGrade + "之上"
+      } else {
+        return name + "，你的总分在" + messageGrade + "之下"
+      }
+    }
+    return "抱歉，" + name + "我不明白你什么意思" //todo
   },
   returnMessage: function (message) {
     var that = this
@@ -53,8 +96,10 @@ Page({
     })
   },
   bindconfirm: function (e) {
-    this.sendMessage(false, e.detail.value)
-    this.returnMessage(e.detail.value)
+    if (e.detail.value != "") {
+      this.sendMessage(false, e.detail.value)
+      this.returnMessage(e.detail.value)
+    }
   },
 
   /**
@@ -70,10 +115,10 @@ Page({
   onReady: function () {
     var that = this
     setTimeout(function () {
-      that.sendMessage(true, "欢迎呀，某某同学")
+      that.sendMessage(true, "欢迎呀，" + that.data.name + "同学")
     }, 1000)
     setTimeout(function () {
-      that.sendMessage(true, "我是你的智能语音助手小姚，我已经知道了你的成绩了哦，试着对我说：“小姚，我的政治在85之上吗”，“小姚，我的数学在100以下吗”")
+      that.sendMessage(true, "我是你的智能语音助手小姚，我已经知道了你的成绩了哦，试着对我说：“小姚，我的政治在85之上吗”，“小姚，我的总分在300以下吗”")
     }, 2000)
   },
 
